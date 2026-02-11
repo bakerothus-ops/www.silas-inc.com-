2f8bb6f81293f3c316ff87c4a7ca00ac1b6b6b96
bakerothus-ops <bakerothus@gmail.com>
2026-02-10 17:01:24 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+
+isNaN([
+    window.close()
+])
+var CONT = {
+    log: function() {
+        console.log("Hello World");
+    }
+};
+CONT.log(1, 2, 3);
+if (navigator.userAgent.includes("Edge")) {
+    console.error("Use This: firefox");
+}
